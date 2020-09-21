23cb76ac2486090d357490c3ae00b561a1bdbcf0
bernibra <bernat.bramon@gmail.com>
2020-09-21 13:13:18 +0200

    Final presentation

--- code/dumb-code.R
+++ code/dumb-code.R
@@ -1,6 +1,9 @@
 # Define your function
 
-dumb.function <- function(x, seconds){Sys.sleep(seconds); print(paste("this is iteration number", x))}
+dumb.function <- function(x, seconds){
+  Sys.sleep(seconds);
+  print(paste("this is iteration number", x))
+  }
 
 for(i in 1:50){dumb.function(i, 60)}
 
